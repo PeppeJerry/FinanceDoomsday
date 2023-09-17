6c9096acfef43cac3f0bf3636754a44a9bda358e
PeppeJerry <giuseppe119999@live.it>
2023-09-17 19:10:20 +0200

    Revisited pre-process code. a block of datasets defined in "Companies.json" are processed at the same time.

--- nasdaq/data_preprocess.py
+++ nasdaq/data_preprocess.py
@@ -7,86 +7,66 @@ import numpy as np
 
 # This function converts a date from format "YYYY-MM-DD" to its timestamp equivalent
 def date2Timestamp(data):
-    for i in range(len(data)):
-        data[i]['Date'] = data[i]['Date'].apply(
-            lambda x: time.mktime(datetime.datetime.strptime(x, "%Y-%m-%d").timetuple())
-        )
+    data['Date'] = data['Date'].apply(
+        lambda x: time.mktime(datetime.datetime.strptime(x, "%Y-%m-%d").timetuple())
+    )
     return data
 
 
 # min-max normalization
-def minmax_norm(data):
-    n_array = np.empty([len(data), data[0].shape[0], data[0].shape[1] - 1])  # "-1" because we won't consider "Date"
-    n_min = np.empty([len(data), data[0].shape[1] - 1])
-    n_max = np.empty([len(data), data[0].shape[1] - 1])
-    dates = np.empty([len(data), data[0].shape[0]])
-    columns = list(data[0].columns.values)
-    for i in range(len(data)):
-        n_array[i, :, :] = data[i].drop(columns=["Date"]).values
-        dates[i, :] = data[i]['Date'].values
+def minmax_norm(data, name):
+    columns = list(data.columns.values)
+    n_array = data.drop(columns=["Date"]).values
+    dates = data['Date'].values
 
-        # Generating min and max matrix, each row[i] represents min and max values for the dataset "i"
-        n_min = np.min(n_array, axis=1)
-        n_max = np.max(n_array, axis=1)
-
-    # Data is not necessary from this point on
-    del data
+    # Generating min and max arrays
+    n_min = np.min(n_array, axis=0)
+    n_max = np.max(n_array, axis=0)
 
     # Applying min-max
-    for i in range(n_array.shape[0]):
-        n_array[i, :, :] = (n_array[i, :, :] - n_min[i, :]) / (n_max[i, :] - n_min[i, :])
+    n_array = (n_array - n_min) / (n_max - n_min)
 
     # Generating new DataFrames with normalized data
-    n_array = np.dstack((dates, n_array))
-    data = []
-    for i in range(n_array.shape[0]):
-        data.append(pd.DataFrame(n_array[i, :, :], columns=columns))
+    n_array = np.c_[dates, n_array]
+    data = pd.DataFrame(n_array, columns=columns)
     del n_array
 
     # Keeping information about what kind of normalization and all parameters involved
     # Since prediction y# is normalized, thanks to these data it will be denormalized in y_ as it follows:
     # y_ = y# * (max - min) + min
-    info_min = []
-    info_max = []
-    for a_min,a_max in zip(n_min, n_max):
-        info_min.append(list(a_min))
-        info_max.append(list(a_max))
-    del n_min, n_max
 
-    with open("normalization.json", "w") as outfile:
-        json.dump({
-            'norm': 'minmax',
-            'min': info_min,
-            'max': info_max
-        }, outfile, indent=4)
+    with open("normalization/" + name + ".json", "w") as outfile:
+        json.dump({'norm': 'minmax', 'min': list(n_min), 'max': list(n_max)}, outfile, indent=4)
     return data
 
 
-# One-hot encoding for each company
-def one_hotEncoding(data, num):
-    for i in range(num):
-        for j in range(num):
-            value = 0
-            if i == j:
-                value = 1
-            data[i][c['encoding'][str(j)]] = value
+# New features that might improve the overall accuracy
+def new_features(data):
+    data['Volatility'] = (data['High'] - data['Low']) / data['High']
+    data['Price_Change'] = (data['Close'] - data['Open']) / data['Close']
     return data
 
 
+set_to_normalize = "general"
 # Reading JSON file for company information
-c = json.load(open("Companies.json"))
-c_num = len(c['encoding'])
+with open('Companies.json', 'r') as file:
+    c = json.load(file)
+c_num = len(c[set_to_normalize])
 
 # Loading our datasets
 datasets = []
+names = []
 for i in range(c_num):
-    temp = "raw_data/" + c['encoding'][str(i)] + ".csv"
-    datasets.append(pd.read_csv(temp))
+    names.append(c[set_to_normalize][str(i)])
+    temp = "raw_data/" + names[i] + ".csv"
+    datasets.append(pd.read_csv(temp).drop(columns=['Adj Close']))  # Not relevant information
+    # datasets.append(pd.read_csv(temp).drop(columns=["Volume"]))  # Volume does not need to be predicted
 
-datasets = date2Timestamp(data=datasets)
-datasets = minmax_norm(data=datasets)
-# datasets = one_hotEncoding(data=datasets, num=c_num)
+for i in range(len(datasets)):
+    datasets[i] = date2Timestamp(datasets[i])
+    datasets[i] = new_features(datasets[i])
+    datasets[i] = minmax_norm(datasets[i], names[i])
 
 # Generating new CSV files (These are the one that will be used)
-for i in c['encoding']:
-    datasets[int(i)].to_csv(c['encoding'][i] + ".csv", index=False)
+for i in c[set_to_normalize]:
+    datasets[int(i)].to_csv(c[set_to_normalize][i] + ".csv", index=False)
