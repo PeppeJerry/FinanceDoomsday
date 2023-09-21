997ee17b5950441eaaf5db2cf531017b7e8329bc
PeppeJerry <giuseppe119999@live.it>
2023-09-21 18:40:14 +0200

    Dropout added plus minor changes

--- models/CNN_BiLSTM.py
+++ models/CNN_BiLSTM.py
@@ -4,13 +4,20 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 
 
+def denormalize(x_norm, norm="minmax", min=6, max=9, mean=4, std=2):
+    if norm == "minmax":
+        x_pred = x_norm * (max - min) + min
+        return x_pred
+
+
 class CNNBiLSTM(nn.Module):
-    def __init__(self, inputDim, out_target, bi_lstm_layers=2, CNN_out=256, dropout=0.5, company='general',
-                 sequence_length=128, SEED=19031999):
+    def __init__(self, inputDim, out_target, bi_lstm_layers=2, CNN_out=256, dropout=0.5, dropout_input=0,
+                 specific='general', sequence_length=128, SEED=-1):
         super(CNNBiLSTM, self).__init__()
-        random = torch.randint(1, 1000000000, (1,)).item()
-        torch.manual_seed(SEED + random)
-        self.company = company
+        if SEED == -1:
+            SEED = torch.randint(1, 10000000000000, (1,)).item()
+        torch.manual_seed(SEED)
+        self.specific = specific
 
         # Defining target time-steps
         self.out_steps = out_target
@@ -57,33 +64,45 @@ class CNNBiLSTM(nn.Module):
             num_layers=bi_lstm_layers,
             batch_first=True
         )
+        self.LSTM_weigth_init(self.bi_lstm)
 
         # Layer Normalization 2
         self.layer_norm2 = nn.LayerNorm(normalized_shape=2 * CNN_out)
 
         # Fully connected
-        self.fc = nn.Linear(2 * CNN_out, inputDim)  # Double because it has forward and backward hidden states
+        self.fc = nn.Linear(2 * CNN_out, 4)  # Double because it has forward and backward hidden states
+        # Volatility & Price change won't be considered
         self.FC_weigth_init(self.fc, temp_length)  # Weights initialization of fc
 
         #  Dropout to reduce overfitting
         self.dropout = nn.Dropout(dropout)
+        self.dropout_input = nn.Dropout(dropout_input)
 
     def forward(self, x):
+        x = self.dropout_input(x)
         x = x.permute(0, 2, 1)  # (batch_size, steps, channels) -> (batch_size, channels, steps)
         x = self.cnn1(x)
+
         x = self.cnn2(x)
 
         x = x.permute(0, 2, 1)  # (batch_size, channels, steps) -> (batch_size, steps, channels)
         x = self.layer_norm1(x)
 
+        x = self.dropout(x)
+
         x, _ = self.bi_lstm(x)
 
         x = self.layer_norm2(x)
 
+        x = self.dropout(x)
+
         x = self.fc(x)
         return x
 
-    def train_model(self, train_loader, x_valid, Y_valid, epochs=200, lr=0.005, lambd=0.05,
+    def set_company(self, company):
+        self.specific = company
+
+    def train_model(self, train_loader, x_valid, Y_valid, epochs=1000, lr=0.001, lambd=0.02,
                     path="models/CNN_BiLSTM_Weights/", extra=""):
         # Defining if CUDA is available for processing, use CPU otherwise
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -137,6 +156,7 @@ class CNNBiLSTM(nn.Module):
             # These variables will always be placed in the first 4 places so it possible to exploit this information
             # NOTE: the model is still taking variables such as "Volume" as input
             loss = criterion(output[:, self.out_steps, :4], Y_batch[:, self.out_steps, :4])
+            #loss = criterion(output[:, :, :4], Y_batch[:, :, :4])
 
             losses.append(loss.item())
             if loss.item() < loss_counter:
@@ -153,13 +173,13 @@ class CNNBiLSTM(nn.Module):
                 if loss_val.item() < loss_val_counter:
                     loss_val_counter = loss_val.item()
                     print("Validation [{},{}]".format(epoch + 1, loss_val_counter))
-                    torch.save(self.state_dict(), path + self.company + extra + '.pth')
+                    torch.save(self.state_dict(), path + self.specific + extra + '.pth')
 
             loss.backward()
             optimizer.step()
 
         # Taking the best parameters obtained for the validation set
-        torch.save(self.state_dict(), path + self.company + extra + '.pth')
+        self.load_state_dict(torch.load(path + self.specific + extra + '.pth'))
         self.eval()
         self.switch_CPU()
         return losses, losses_val
@@ -170,6 +190,10 @@ class CNNBiLSTM(nn.Module):
             criterion = nn.MSELoss()
             loss = criterion(output, Y)
             return loss.item()
+
+    def denormalize(self, x, min_vect=6, max_vect=9, mean=4, std=2):
+        y_norm = self(x)
+        return denormalize(y_norm, min=min_vect, max=max_vect)
 
     def switch_CPU(self):
         device = torch.device("cpu")
@@ -218,3 +242,8 @@ class CNNBiLSTM(nn.Module):
         weight_coefficient = torch.sqrt(weight_coefficient).item()
         module.weight.data = torch.randn(module.weight.data.size()) * weight_coefficient
         module.bias.data = torch.randn(module.bias.data.size()) * weight_coefficient
+
+    def LSTM_weigth_init(self, module):
+        for name, param in module.named_parameters():
+            if "weight" in name:
+                nn.init.xavier_normal_(param)
