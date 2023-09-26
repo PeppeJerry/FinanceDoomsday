62c22f6f19b5fccce95a053b56f6fd7066a89fab
PeppeJerry <giuseppe119999@live.it>
2023-09-27 01:38:51 +0200

    Minor changes, more comments

--- models/CNN_BiLSTM_autoReg.py
+++ models/CNN_BiLSTM_autoReg.py
@@ -4,29 +4,26 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
-
-
-def denormalize(x_norm, norm="minmax", min=6, max=9, mean=4, std=2):
-    if norm == "minmax":
-        x_pred = np.zeros(x_norm.shape)
-        for i in range(x_norm.shape[0]):
-            x_pred[i, :, :] = x_norm[i, :, :] * (max - min) + min
-        return x_pred
+from models.xavier import CNN_weight_init, FC_weigth_init, LSTM_weigth_init
 
 
 class CNNBiLSTM(nn.Module):
     def __init__(self, inputDim, out_target, bi_lstm_layers=2, CNN_out=256, dropout=0, dropout_input=0,
-                 specific='general', sequence_length=128, SEED=-1):
+                 specific='general', sequence_length=128, SEED=-1, extra="", lr=0.1, lmd=0.001, outLen=14):
         super(CNNBiLSTM, self).__init__()
         if SEED == -1:
             SEED = torch.randint(1, 10000000000000, (1,)).item()
         torch.manual_seed(SEED)
         self.device = torch.device("cpu")
 
+        self.extra = extra
+        self.lr = lr
+        self.lmd = lmd
         self.specific = specific
 
         # Defining target time-steps
         self.out_steps = out_target
+        self.outLen = outLen
 
         # CNN Layers to discover patterns in our sequence
         # First CNN Block
@@ -42,7 +39,7 @@ class CNNBiLSTM(nn.Module):
         for module in self.cnn1:
             if not (isinstance(module, nn.Conv1d)):
                 continue
-            temp_length = self.CNN_weight_init(module, temp_length)
+            temp_length, module = CNN_weight_init(module, temp_length)
 
         # Second CNN Block (Resizing desired dimensionality)
         self.cnn2 = nn.Sequential(
@@ -56,7 +53,7 @@ class CNNBiLSTM(nn.Module):
         for module in self.cnn2:
             if not (isinstance(module, nn.Conv1d)):
                 continue
-            temp_length = self.CNN_weight_init(module, temp_length)
+            temp_length, module = CNN_weight_init(module, temp_length)
 
         self.cnn3 = nn.Sequential(
             nn.Conv1d(in_channels=128, out_channels=128, kernel_size=4, padding=1, stride=2),
@@ -69,7 +66,7 @@ class CNNBiLSTM(nn.Module):
         for module in self.cnn3:
             if not (isinstance(module, nn.Conv1d)):
                 continue
-            temp_length = self.CNN_weight_init(module, temp_length)
+            temp_length, module = CNN_weight_init(module, temp_length)
 
         # Layer Normalization 1
         self.layer_norm1 = nn.LayerNorm(normalized_shape=CNN_out)
@@ -83,7 +80,8 @@ class CNNBiLSTM(nn.Module):
             num_layers=bi_lstm_layers,
             batch_first=True
         )
-        self.LSTM_weigth_init(self.bi_lstm)
+        # Weights initialization of bi_lstm
+        self.bi_lstm = LSTM_weigth_init(self.bi_lstm)
 
         # Layer Normalization 2
         self.layer_norm2 = nn.LayerNorm(normalized_shape=2 * CNN_out)
@@ -91,17 +89,22 @@ class CNNBiLSTM(nn.Module):
         # Fully connected
         self.fc = nn.Linear(2 * CNN_out, 4)  # Double because it has forward and backward hidden states
         # Volatility & Price change won't be considered
-        self.FC_weigth_init(self.fc, temp_length)  # Weights initialization of fc
+        self.fc = FC_weigth_init(self.fc, temp_length)  # Weights initialization of fc
 
         #  Dropout to reduce overfitting
         self.dropout = nn.Dropout(dropout)
         self.dropout_input = nn.Dropout(dropout_input)
 
     def forward(self, x):
-        x_temp = x
-        y = torch.zeros(x.size(0), 14, x.size(2)).to(self.device)
-        for i in range(14):
 
+        # In order to make the algorithm autoregressive, the original sequence x will slide one day at a time
+        # The output of each iteration will become the new input until self.outLen outputs are generated
+        x_temp = x
+        y = torch.zeros(x.size(0), self.outLen, x.size(2)).to(self.device)
+
+        hn = torch.zeros(0, 0, 0).to(self.device)
+        cn = torch.zeros(0, 0, 0).to(self.device)
+        for i in range(self.outLen):
             if i != 0:
                 temp_1 = x_temp[:, 1:, :]
                 temp_2 = y[:, :i, :]
@@ -114,12 +117,19 @@ class CNNBiLSTM(nn.Module):
             x = self.cnn2(x)
             x = self.cnn3(x)
 
-            x = x.permute(0, 2, 1)  # (batch_size, channels, steps) -> (batch_size, steps, channels)
+            x = x.permute(0, 2, 1)  # (batch_size, channels, 1) -> (batch_size, 1, channels)
             x = self.layer_norm1(x)
 
             x = self.dropout(x)
 
-            x, _ = self.bi_lstm(x)
+            # Due to CNN layers, the input has dimensionality (Batch_size, 1, channels)
+            # In order to efficiently use the recursive architecture, hidden and cell states of previous iterations
+            # will be available for the next iteration (i+1)
+            if i == 0:
+                # If first iteration then they are set by default (Might be improved if hn and cn are trained as well)
+                x, (hn, cn) = self.bi_lstm(x)
+            else:
+                x, (hn, cn) = self.bi_lstm(x, (hn, cn))
 
             x = self.layer_norm2(x)
 
@@ -130,23 +140,20 @@ class CNNBiLSTM(nn.Module):
             y[:, i, :4] = x[:, 0, :4]
         return y
 
-    def set_company(self, company):
-        self.specific = company
-
-    def train_model(self, train_loader, x_valid, Y_valid, epochs=500, lr=0.001, lambd=0,
-                    path="models/CNN_BiLSTM_Weights/", extra=""):
+    def train_model(self, train_loader, x_valid, Y_valid, epochs=2000,
+                    path="models/CNN_BiLSTM_Weights/", extra="", custom_loss=False, threshold=100):
         # Defining if CUDA is available for processing, use CPU otherwise
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.to(device)
         self.device = device
-
+        lr = self.lr
         # If "cuda" is available data will move accordingly
         x_valid = x_valid.to(device)
         Y_valid = Y_valid.to(device)
 
         # AdamW optimizer
         # Better L2 Regularization compared to Adam
-        optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=lambd)
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=self.lmd)
 
         # MSE loss
         criterion = nn.MSELoss()
@@ -163,7 +170,9 @@ class CNNBiLSTM(nn.Module):
         losses_val = []
         batch_iter = iter(train_loader)
 
+        LowLR = 0
         for epoch in range(epochs):
+            LowLR = LowLR + 1
             optimizer.zero_grad()
 
             # Checking if iterator has a batch for the iteration
@@ -179,16 +188,19 @@ class CNNBiLSTM(nn.Module):
 
             output = self(x_batch)
 
-            # Personalized loss function
+            if custom_loss:
+                # Personalized loss function
 
-            # 1) Loss will be evaluated for indexes self.out_steps
-            # if self.out_steps = [0,6,13] is the same as increasing predict accuracies on 1 day, 1 week, 2 weeks
+                # 1) Loss will be evaluated for indexes self.out_steps
+                # if self.out_steps = [0,6,13] is the same as increasing predict accuracies on 1 day, 1 week, 2 weeks
 
-            # 2) Loss will not consider all variables since we are interested in (Open, Close, High, Low)
-            # These variables will always be placed in the first 4 places so it possible to exploit this information
-            # NOTE: the model is still taking variables such as "Volume" as input
-            # loss = criterion(output[:, self.out_steps, :4], Y_batch[:, self.out_steps, :4])
-            loss = criterion(output[:, :, :4], Y_batch[:, :, :4])
+                # 2) Loss will not consider all variables since we are interested in (Open, Close, High, Low)
+                # These variables will always be placed in the first 4 places so it possible to exploit this information
+                # NOTE: the model is still taking variables such as "Volume" as input
+                loss = criterion(output[:, self.out_steps, :4], Y_batch[:, self.out_steps, :4])
+            else:
+                # Standard loss by considering just the first 4 features (Open, Close, High, Low) for better accuracy
+                loss = criterion(output[:, :, :4], Y_batch[:, :, :4])
 
             losses.append(loss.item())
             if loss.item() < loss_counter:
@@ -204,12 +216,19 @@ class CNNBiLSTM(nn.Module):
 
                 # Saving best parameters based on validation set
                 if loss_val.item() < loss_val_counter:
+                    LowLR = 0
+                    threshold = max(100, int(threshold * 0.05))
                     loss_val_counter = loss_val.item()
                     print("Validation [{},{}]".format(epoch + 1, loss_val_counter))
                     torch.save(self.state_dict(), path + self.specific + extra + '.pth')
 
             loss.backward()
             optimizer.step()
+
+            if LowLR >= threshold:
+                lr = lr / 2
+                optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=self.lmd)
+                LowLR = 0
 
         # Taking the best parameters obtained for the validation set
         self.load_state_dict(torch.load(path + self.specific + extra + '.pth'))
@@ -224,10 +243,6 @@ class CNNBiLSTM(nn.Module):
             loss = criterion(output, Y)
             return loss.item()
 
-    def denormalize(self, x, min_vect=6, max_vect=9, mean=4, std=2):
-        y_norm = self(x)
-        return denormalize(y_norm, min=min_vect, max=max_vect)
-
     def switch_CPU(self):
         device = torch.device("cpu")
         self.to(device)
@@ -239,46 +254,3 @@ class CNNBiLSTM(nn.Module):
         self.to(device)
         self.device = device
         return torch.cuda.is_available()
-
-    def CNN_weight_init(self, module, sequence_length):
-
-        # Number of filters
-        out_dim = module.out_channels
-
-        # Input channels
-        inputDim = module.in_channels
-
-        # Config parameters of nn.Conv1d() module
-        F = module.kernel_size[0]
-        P = module.padding[0]
-        S = module.stride[0]
-
-        # Evaluating fan-in (n_in) and fan-out (n_out)
-        n_in = sequence_length * inputDim
-        temp_length = int((sequence_length + 2 * P - F) / S + 1)  # Feature map dimension
-        n_out = int(temp_length * out_dim)
-
-        # Applying Xavier (Glorot) initialization
-        weight_coefficient = torch.tensor(2 / (n_in + n_out))
-        weight_coefficient = torch.sqrt(weight_coefficient).item()
-        module.weight.data = torch.randn(module.weight.data.size()) * weight_coefficient
-        module.bias.data = torch.randn(module.bias.data.size()) * weight_coefficient
-
-        # Returning the new sequence length after applying convolution
-        return temp_length
-
-    def FC_weigth_init(self, module, sequence_length):
-        # Evaluating fan-in (n_in) and fan-out (n_out)
-        n_in = sequence_length * module.in_features
-        n_out = sequence_length * module.out_features
-
-        # Applying Xavier (Glorot) initialization
-        weight_coefficient = torch.tensor(2 / (n_in + n_out))
-        weight_coefficient = torch.sqrt(weight_coefficient).item()
-        module.weight.data = torch.randn(module.weight.data.size()) * weight_coefficient
-        module.bias.data = torch.randn(module.bias.data.size()) * weight_coefficient
-
-    def LSTM_weigth_init(self, module):
-        for name, param in module.named_parameters():
-            if "weight" in name:
-                nn.init.xavier_normal_(param)
