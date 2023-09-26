f12b77afb33566834f65f42b2b093ad40dcce1a1
PeppeJerry <giuseppe119999@live.it>
2023-09-26 21:16:50 +0200

    Minor changes, more comments. Learning rate lr now decreases if both training loss and validation loss do not decrease after "threshold" iterations

--- models/CNN_BiLSTM.py
+++ models/CNN_BiLSTM.py
@@ -2,18 +2,22 @@ import json
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
-
-
-def denormalize(x_norm, norm="minmax", min=6, max=9, mean=4, std=2):
-    if norm == "minmax":
-        x_pred = x_norm * (max - min) + min
-        return x_pred
+from models.xavier import CNN_weight_init, FC_weigth_init, LSTM_weigth_init
 
 
 class CNNBiLSTM(nn.Module):
     def __init__(self, inputDim, out_target, bi_lstm_layers=2, CNN_out=256, dropout=0.5, dropout_input=0,
-                 specific='general', sequence_length=128, SEED=-1):
+                 specific='general', sequence_length=128, SEED=-1, extra="", lr=0.1, lmd=0.001):
         super(CNNBiLSTM, self).__init__()
+
+        self.extra = extra
+        self.lr = lr
+        self.lmd = lmd
+        self.drop_prop = dropout
+        self.drop_propIN = dropout_input
+        self.specific = specific
+
+        self.device = torch.device("cpu")
         if SEED == -1:
             SEED = torch.randint(1, 10000000000000, (1,)).item()
         torch.manual_seed(SEED)
@@ -36,7 +40,7 @@ class CNNBiLSTM(nn.Module):
         for module in self.cnn1:
             if not (isinstance(module, nn.Conv1d)):
                 continue
-            temp_length = self.CNN_weight_init(module, temp_length)
+            temp_length, module = CNN_weight_init(module, temp_length)
 
         # Second CNN Block (Resizing desired dimensionality)
         self.cnn2 = nn.Sequential(
@@ -50,7 +54,7 @@ class CNNBiLSTM(nn.Module):
         for module in self.cnn2:
             if not (isinstance(module, nn.Conv1d)):
                 continue
-            temp_length = self.CNN_weight_init(module, temp_length)
+            temp_length, module = CNN_weight_init(module, temp_length)
 
         # Layer Normalization 1
         self.layer_norm1 = nn.LayerNorm(normalized_shape=CNN_out)
@@ -64,7 +68,7 @@ class CNNBiLSTM(nn.Module):
             num_layers=bi_lstm_layers,
             batch_first=True
         )
-        self.LSTM_weigth_init(self.bi_lstm)
+        self.bi_lstm = LSTM_weigth_init(self.bi_lstm)
 
         # Layer Normalization 2
         self.layer_norm2 = nn.LayerNorm(normalized_shape=2 * CNN_out)
@@ -72,7 +76,7 @@ class CNNBiLSTM(nn.Module):
         # Fully connected
         self.fc = nn.Linear(2 * CNN_out, 4)  # Double because it has forward and backward hidden states
         # Volatility & Price change won't be considered
-        self.FC_weigth_init(self.fc, temp_length)  # Weights initialization of fc
+        self.fc = FC_weigth_init(self.fc, temp_length)  # Weights initialization of fc
 
         #  Dropout to reduce overfitting
         self.dropout = nn.Dropout(dropout)
@@ -99,14 +103,13 @@ class CNNBiLSTM(nn.Module):
         x = self.fc(x)
         return x
 
-    def set_company(self, company):
-        self.specific = company
-
-    def train_model(self, train_loader, x_valid, Y_valid, epochs=400, lr=0.001, lambd=0.02,
-                    path="models/CNN_BiLSTM_Weights/", extra=""):
+    def train_model(self, train_loader, x_valid, Y_valid, epochs=400,
+                    path="models/CNN_BiLSTM_Weights/", extra="", custom_loss=False, threshold=100):
         # Defining if CUDA is available for processing, use CPU otherwise
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.to(device)
+
+        lr = self.lr
 
         # If "cuda" is available data will move accordingly
         x_valid = x_valid.to(device)
@@ -114,7 +117,7 @@ class CNNBiLSTM(nn.Module):
 
         # AdamW optimizer
         # Better L2 Regularization compared to Adam
-        optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=lambd)
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=self.lmd)
 
         # MSE loss
         criterion = nn.MSELoss()
@@ -131,9 +134,10 @@ class CNNBiLSTM(nn.Module):
         losses_val = []
         batch_iter = iter(train_loader)
 
+        LowLR = 0
         for epoch in range(epochs):
             optimizer.zero_grad()
-
+            LowLR = LowLR + 1
             # Checking if iterator has a batch for the iteration
             try:
                 x_batch, Y_batch = next(batch_iter)
@@ -147,23 +151,25 @@ class CNNBiLSTM(nn.Module):
 
             output = self(x_batch)
 
-            # Personalized loss function
+            if custom_loss:
+                # Personalized loss function
 
-            # 1) Loss will be evaluated for indexes self.out_steps
-            # if self.out_steps = [0,6,13] is the same as increasing predict accuracies on 1 day, 1 week, 2 weeks
+                # 1) Loss will be evaluated for indexes self.out_steps
+                # if self.out_steps = [0,6,13] is the same as increasing predict accuracies on 1 day, 1 week, 2 weeks
 
-            # 2) Loss will not consider all variables since we are interested in (Open, Close, High, Low)
-            # These variables will always be placed in the first 4 places so it possible to exploit this information
-            # NOTE: the model is still taking variables such as "Volume" as input
-            # loss = criterion(output[:, self.out_steps, :4], Y_batch[:, self.out_steps, :4])
-
-            # Normal loss on (Open, Close, High, Low)
-            loss = criterion(output[:, :, :4], Y_batch[:, :, :4])
+                # 2) Loss will not consider all variables since we are interested in (Open, Close, High, Low)
+                # These variables will always be placed in the first 4 places so it possible to exploit this information
+                # NOTE: the model is still taking variables such as "Volume" as input
+                loss = criterion(output[:, self.out_steps, :4], Y_batch[:, self.out_steps, :4])
+            else:
+                # Standard loss by considering just the first 4 features (Open, Close, High, Low) for better accuracy
+                loss = criterion(output[:, :, :4], Y_batch[:, :, :4])
 
             losses.append(loss.item())
             if loss.item() < loss_counter:
+                LowLR = 0
                 loss_counter = loss.item()
-                print("Training [{},{}]".format(epoch + 1, loss_counter))
+                print("Training   [{},{:.13f}]".format(epoch + 1, loss_counter))
 
             # The overall training process will not consider these lines below
             with torch.no_grad():
@@ -173,12 +179,20 @@ class CNNBiLSTM(nn.Module):
 
                 # Saving best parameters based on validation set
                 if loss_val.item() < loss_val_counter:
+                    LowLR = 0
                     loss_val_counter = loss_val.item()
-                    print("Validation [{},{}]".format(epoch + 1, loss_val_counter))
+                    print("Validation [{},{:.13f}]".format(epoch + 1, loss_val_counter))
                     torch.save(self.state_dict(), path + self.specific + extra + '.pth')
 
             loss.backward()
             optimizer.step()
+
+            # Decreasing Learning Rate if the model is not improving after "threshold" iterations
+            if LowLR >= threshold:
+                LowLR = 0
+                print("Lowering lr [{} -> {}]".format(lr, lr / 10))
+                lr = lr / 10
+                optimizer = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=self.lmd)
 
         # Taking the best parameters obtained for the validation set
         self.load_state_dict(torch.load(path + self.specific + extra + '.pth'))
@@ -193,59 +207,14 @@ class CNNBiLSTM(nn.Module):
             loss = criterion(output, Y)
             return loss.item()
 
-    def denormalize(self, x, min_vect=6, max_vect=9, mean=4, std=2):
-        y_norm = self(x)
-        return denormalize(y_norm, min=min_vect, max=max_vect)
-
     def switch_CPU(self):
         device = torch.device("cpu")
         self.to(device)
+        self.device = device
         return True
 
     def switch_CUDA(self):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.to(device)
+        self.device = device
         return torch.cuda.is_available()
-
-    def CNN_weight_init(self, module, sequence_length):
-
-        # Number of filters
-        out_dim = module.out_channels
-
-        # Input channels
-        inputDim = module.in_channels
-
-        # Config parameters of nn.Conv1d() module
-        F = module.kernel_size[0]
-        P = module.padding[0]
-        S = module.stride[0]
-
-        # Evaluating fan-in (n_in) and fan-out (n_out)
-        n_in = sequence_length * inputDim
-        temp_length = int((sequence_length + 2 * P - F) / S + 1)  # Feature map dimension
-        n_out = int(temp_length * out_dim)
-
-        # Applying Xavier (Glorot) initialization
-        weight_coefficient = torch.tensor(2 / (n_in + n_out))
-        weight_coefficient = torch.sqrt(weight_coefficient).item()
-        module.weight.data = torch.randn(module.weight.data.size()) * weight_coefficient
-        module.bias.data = torch.randn(module.bias.data.size()) * weight_coefficient
-
-        # Returning the new sequence length after applying convolution
-        return temp_length
-
-    def FC_weigth_init(self, module, sequence_length):
-        # Evaluating fan-in (n_in) and fan-out (n_out)
-        n_in = sequence_length * module.in_features
-        n_out = sequence_length * module.out_features
-
-        # Applying Xavier (Glorot) initialization
-        weight_coefficient = torch.tensor(2 / (n_in + n_out))
-        weight_coefficient = torch.sqrt(weight_coefficient).item()
-        module.weight.data = torch.randn(module.weight.data.size()) * weight_coefficient
-        module.bias.data = torch.randn(module.bias.data.size()) * weight_coefficient
-
-    def LSTM_weigth_init(self, module):
-        for name, param in module.named_parameters():
-            if "weight" in name:
-                nn.init.xavier_normal_(param)
